9f37d2f3e86df6bf32e7c850bf7d420d850e93ce
Fredrik bjornland <43269579+Fredrikbjornland@users.noreply.github.com>
2021-03-09 11:48:14 +0100

    add a main file for testing, update rule and fsm.py

--- rule.py
+++ rule.py
@@ -2,7 +2,10 @@
 class Rule:
     """ Rule class """
     def __init__(self, s1, s2, signal, action):
-        self.state1, self.state2, self.signal, self.action = s1, s2, signal, action
+        self.state1 = s1
+        self.state2 = s2
+        self.signal = signal
+        self.action = action
 
     def match(self):
         pass
