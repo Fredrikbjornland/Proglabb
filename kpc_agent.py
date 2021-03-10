277eb482ae90a372d46bd7ad4ab3947ac60a0887
Fredrik bjornland <43269579+Fredrikbjornland@users.noreply.github.com>
2021-03-10 12:11:46 +0100

    fixed pylint score for fsm and kpc_agent

--- kpc_agent.py
+++ kpc_agent.py
@@ -10,26 +10,27 @@ class KPCAgent:
         self.current_password_buffer = ""
         self.override_signal = None
         self.lid = 0
-        self.LDur = 0
+        self.l_dur = 0
 
     def reset_password_entry(self, signal):
-        """- Clear the password-buffer and 
+        """ Clear the password-buffer and
         initiate a “power up” lighting
         sequence on the LED Board."""
         self.current_password_buffer = ""
         self.led_board.power_up()
 
     def get_next_signal(self):
-        """ Return the override signal, if it is non-blank; 
+        """ Return the override signal, if it is non-blank;
         otherwise query the keypad for the next pressed key. """
         print("Type in the next signal: ")
-        if self.override_signal != None:
+        if self.override_signal is not None:
             holder = self.override_signal
             self.override_signal = None
             return holder
         return self.keypad.get_next_signal()
 
     def append_next_password_digit(self, signal):
+        """ Append the signal to the current password """
         print("Append next password digit")
         print("signal", signal)
         if signal:
@@ -72,28 +73,34 @@ class KPCAgent:
                 self.led_board.successfull()
                 """ Initiate successfull LED lights """
             writer.close()
-    def setLid(self, lid):
+    def set_lid(self, lid):
+        """ Set id of the LED that will turn on """
         print("Set lid")
         self.lid = lid
 
-    def setLDur(self, LDur):
+    def set_l_dur(self, l_dur):
+        """ Set duration of led """
         print("set dur")
-        self.LDur = LDur
-    
+        self.l_dur = l_dur
+
     def light_one_led(self, signal):
+        """ Light the LED """
         print("light led")
-        self.led_board.light_led(int(self.lid), int(self.LDur))
+        self.led_board.light_led(int(self.lid), int(self.l_dur))
 
     def reset_agent(self, signal):
+        """ Reset agent """
         print("Reset agent")
         self.reset_password_entry("sdf")
         self.override_signal = None
 
     def power_down(self, signal):
+        """ Power down"""
         self.reset_agent(signal)
         print("Powering down")
         self.led_board.power_down()
 
     def fully_active_agent(self, signal):
+        """ Agent is logged in and active"""
         print("Active agent")
 
